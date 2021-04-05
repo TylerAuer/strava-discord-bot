a70a3e4ec34b1b218dc7fd5dc810d902e74a1798
Tyler Auer <Tyler.Auer@gmail.com>
2021-04-04 17:57:52 -0700

    Add Dockerfile

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module go-strava-discord-bot
 go 1.15
 
 require (
+	github.com/aws/aws-lambda-go v1.23.0
 	github.com/bwmarrin/discordgo v0.23.2
 	github.com/joho/godotenv v1.3.0
 )
--- main.go
+++ main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/joho/godotenv"
 )
 
+type MyEvent struct {
+	Name string `json:"name"`
+}
+
 func main() {
+	lambda.Start(HandleRequest)
+}
+
+func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
+	getStravaActivitiesAndPostToDiscord()
+
+	return fmt.Sprintf("Hello %s!", name.Name), nil
+}
+
+func getStravaActivitiesAndPostToDiscord() {
 	godotenv.Load() // Load env vars from ./.env
 
 	st := getStravaAccessToken()
