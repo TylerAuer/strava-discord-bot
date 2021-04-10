e1e1ddaad2de9a8c8767619948caec4fc4ce6c57
Tyler Auer <Tyler.Auer@gmail.com>
2021-04-10 10:53:37 -0700

    Spacing for activity details

--- parse_activity_stats_into_post.go
+++ parse_activity_stats_into_post.go
@@ -5,33 +5,36 @@ import (
 	"strings"
 )
 
-/*
-ID
-<Name> finished a <type>
-<Distance> gaining <elevation gain> in <Moving Time> at <min/mile>
-*/
-
 func parseActivityStatsIntoPost(a ActivityDetails, k Kraftee) string {
 	id := fmt.Sprint(a.ID)
 	url := "https://www.strava.com/activities/" + id
-	dist := "**Dist:** " + fmt.Sprintf("%.1f", metersToMiles(a.Distance))
-	elev := "**Elev:** +" + fmt.Sprintf("%.1f", metersToFeet(a.TotalElevationGain))
-	movTime := "**Time:** " + secondsToHoursMinsSeconds(a.MovingTime)
+	dist := "Distance:        " + fmt.Sprintf("%.1f", metersToMiles(a.Distance)) + " miles"
+	elev := "Elevation:       " + fmt.Sprintf("%.1f", metersToFeet(a.TotalElevationGain)) + " feet gained"
+	movTime := "Time:            " + secondsToHoursMinsSeconds(a.MovingTime)
 	paceInSecondsPerMile := float64(a.MovingTime) / metersToMiles(a.Distance)
 	pace := secondsToMinSec(paceInSecondsPerMile) + " per mile"
 	relativeEffort := func() string {
 		if a.SufferScore == 0 {
 			return ""
-		} else {
-			return "**RelEff:** " + fmt.Sprint(a.SufferScore) + "\n"
 		}
+		return "Relative Effort: " + fmt.Sprint(a.SufferScore) + "\n"
+	}()
+	cals := func() string {
+		if a.Calories == 0 {
+			return ""
+		}
+		return "Calories:        " + fmt.Sprint(a.Calories) + "\n"
 	}()
 
 	return "" +
 		"*" + k.First + " just logged a " + strings.ToLower(a.Type) + "*\n\n" +
+		"**STATS**" +
+		"```" +
 		dist + "\n" +
-		elev + "\n" +
 		movTime + " @ " + pace + "\n" +
+		elev + "\n" +
 		relativeEffort +
+		cals +
+		"```" +
 		"\n" + url
 }
