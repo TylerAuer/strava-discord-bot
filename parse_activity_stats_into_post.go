145d1d5d92e186cad1a375cfbd216e2a5d500686
Tyler Auer <Tyler.Auer@gmail.com>
2021-04-10 10:38:45 -0700

    Clean up post

--- parse_activity_stats_into_post.go
+++ parse_activity_stats_into_post.go
@@ -14,16 +14,16 @@ ID
 func parseActivityStatsIntoPost(a ActivityDetails, k Kraftee) string {
 	id := fmt.Sprint(a.ID)
 	url := "https://www.strava.com/activities/" + id
-	dist := fmt.Sprintf("%.1f", metersToMiles(a.Distance)) + " miles travelled"
-	elev := fmt.Sprintf("%.1f", metersToFeet(a.TotalElevationGain)) + "' gained"
-	movTime := secondsToHoursMinsSeconds(a.MovingTime) + " moving time"
+	dist := "**Dist:** " + fmt.Sprintf("%.1f", metersToMiles(a.Distance))
+	elev := "**Elev:** +" + fmt.Sprintf("%.1f", metersToFeet(a.TotalElevationGain))
+	movTime := "**Time:** " + secondsToHoursMinsSeconds(a.MovingTime)
 	paceInSecondsPerMile := float64(a.MovingTime) / metersToMiles(a.Distance)
 	pace := secondsToMinSec(paceInSecondsPerMile) + " per mile"
 	relativeEffort := func() string {
 		if a.SufferScore == 0 {
 			return ""
 		} else {
-			return "Relative Effort: " + fmt.Sprint(a.SufferScore) + "\n"
+			return "**RelEff:** " + fmt.Sprint(a.SufferScore) + "\n"
 		}
 	}()
 
