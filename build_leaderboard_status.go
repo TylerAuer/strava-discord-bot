87c30f884a1e0e4da48af9c9d53d8add91aa6443
Tyler Auer <Tyler.Auer@gmail.com>
2021-05-30 11:21:37 -0700

    Improve naming

--- build_leaderboard_status.go
+++ build_leaderboard_status.go
@@ -2,23 +2,23 @@ package main
 
 func buildLeaderboardStatus(a ActivityDetails, k Kraftee) string {
 	startOfWeek := getStartOfWeekInUnixTime()
-	kStats, _ := getAllKrafteeStatsSince(startOfWeek)
+	leaderboard, _ := getAllKrafteeStatsSince(startOfWeek)
 
-	lbs := "**Leaderboard** @ post time\n"
-	lbs += "```\n"
-	lbs += kStats.printActivityCountUpToKraftee(k)
-	lbs += kStats.printDurationUpToKraftee(k)
+	postString := "**Leaderboard** @ post time\n"
+	postString += "```\n"
+	postString += leaderboard.printActivityCountUpToKraftee(k)
+	postString += leaderboard.printDurationUpToKraftee(k)
 
 	if a.Type == "Run" {
-		lbs += kStats.printRunDistanceUpToKraftee(k)
-		lbs += kStats.printRunDurationUpToKraftee(k)
+		postString += leaderboard.printRunDistanceUpToKraftee(k)
+		postString += leaderboard.printRunDurationUpToKraftee(k)
 	}
 
 	if a.Type == "Ride" {
-		lbs += kStats.printRideDistanceUpToKraftee(k)
-		lbs += kStats.printRideDurationUpToKraftee(k)
+		postString += leaderboard.printRideDistanceUpToKraftee(k)
+		postString += leaderboard.printRideDurationUpToKraftee(k)
 	}
 
-	lbs += "```"
-	return lbs
+	postString += "```"
+	return postString
 }
