9aa1165d8e2c6f2ed1bfdffa97f0adce96d07009
Tyler Auer <Tyler.Auer@gmail.com>
2021-06-01 20:59:32 -0700

    Add random GIF function

--- handleNagCheck.go
+++ handleNagCheck.go
@@ -1,11 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func handleNagCheck() {
-	// Load all of the data for x amount of time
-	// Check if they have at least 1 activity in that data set
-	// If not, send a nag message
 
 	dg := getActiveDiscordSession()
 	defer dg.Close()
@@ -16,11 +16,65 @@ func handleNagCheck() {
 			activities := getActivitiesSince(dateToStartCheckFrom, k)
 			if len(activities) == 0 {
 				msg := k.First + ", it's been more than "
-				msg += fmt.Sprint(k.daysBeforeNag) + " since your last workout. Man up!\n"
-				msg += "https://media.giphy.com/media/dZcJvBQL503SW5fErx/giphy.gif"
+				msg += fmt.Sprint(k.daysBeforeNag) + " days since your last workout. Man up!\n"
+				msg += getRandomGifOfInspiration()
 
 				postToDiscord(dg, msg)
 			}
 		}
 	}
 }
+
+func getRandomGifOfInspiration() string {
+	gifsOfInspiration := []string{
+		"https://media.giphy.com/media/dZcJvBQL503SW5fErx/giphy.gif",
+		"https://media.giphy.com/media/6XA99Q0nPSXyU/giphy.gif",
+		"https://media.giphy.com/media/mcH0upG1TeEak/giphy.gif",
+		"https://media.giphy.com/media/rfskmSvktqSoo/giphy.gif",
+		"https://media.giphy.com/media/IVUT6ZrmwluToUnb1F/giphy.gif",
+		"https://media.giphy.com/media/pVkmGyqYRt4qY/giphy.gif",
+		"https://media.giphy.com/media/w9DgGBsFfU9OM/giphy.gif",
+		"https://media.giphy.com/media/kaCdrXPD9hkZzsEEJn/giphy.gif",
+		"https://media.giphy.com/media/Ob7p7lDT99cd2/giphy.gif",
+		"https://media.giphy.com/media/d5mI2F3MxCTJu/giphy.gif",
+	}
+
+	randomIndex := rand.Intn(len(gifsOfInspiration))
+
+	return gifsOfInspiration[randomIndex]
+
+}
+
+// func getAllKrafteeStatsSince(startEpochTime int64) (Leaderboard, ActivityList) {
+// 	var lb Leaderboard          // Holds each Kraftees stats for comparison
+// 	var activities ActivityList // Holds all activities for group stats computation
+
+// 	lbChan := make(chan Stats)
+// 	activityListChan := make(chan ActivityList)
+
+// 	for _, k := range krafteesByStravaId {
+// 		go getOneKrafteesStats(startEpochTime, k, lbChan, activityListChan)
+// 	}
+
+// 	// Handle incoming channel messages
+// 	for i := 0; i < 2*len(krafteesByStravaId); i++ {
+// 		select {
+// 		case newKrafteeStats := <-lbChan:
+// 			lb = append(lb, newKrafteeStats)
+// 		case newListOfActivities := <-activityListChan:
+// 			activities = append(activities, newListOfActivities...)
+// 		}
+// 	}
+
+// 	return lb, activities
+// }
+
+// func getOneKrafteesStats(t int64, k Kraftee, lbChan chan Stats, activityListChan chan ActivityList) {
+// 	actList := getActivitiesSince(t, k)
+// 	activityListChan <- actList
+
+// 	kStats := actList.buildStats(k.First, k.StravaId)
+// 	lbChan <- kStats
+
+// 	fmt.Println("Finished " + k.FullName())
+// }
